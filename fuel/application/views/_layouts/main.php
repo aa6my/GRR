0dbfc2b0edd2f66c321a2d3701ea1666eaa5166a
aa6my <aa6my@qq.com>
2015-01-11 01:44:21 +0800

    Games Reviews & Recommendations

---
+++ fuel/application/views/_layouts/main.php
@@ -0,0 +1,7 @@
+<?php $this->load->view('_blocks/header')?>
+	
+	<section id="main_inner">
+		
+	</section>
+	
+<?php $this->load->view('_blocks/footer')?>
---
+++ fuel/application/views/home.php
@@ -0,0 +1,2 @@
+<?php $this->load->view('_layouts/main')?>
+
